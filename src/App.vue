08b756d82c254277aba9b4e1a6f0c3275decfd73
xuhanfeng <13341185132@163.com>
2018-09-03 21:28:28 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header></header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+export default {
+  name: 'App',
+  component:{
+    header
+  }
+}
+</script>
+
+<style>
+#app {
+ 
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+      <h1>音乐app</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../../assets/mixin.scss';
+.hello{
+  width:100%;
+  height: 3rem;
+  background:#ccc;
+  line-height:3rem;
+  text-align:center;
+  font-size:1.2rem;
+}
+</style>
